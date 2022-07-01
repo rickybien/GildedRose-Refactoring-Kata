1aa3953f728399128a27b9225680584472422927
taihua-chen <taihua.chen@kkday.com>
2022-07-01 11:16:37 +0800

    fix on date

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -54,8 +54,11 @@ final class GildedRose
                 if ($item->name != 'Aged Brie') {
                     if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                            if ($item->name != 'Sulfuras, Hand of Ragnaros' && $item->name != 'Conjured') {
                                 $item->quality = $item->quality - 1;
+                            }
+                            if ($item->name === 'Conjured') {
+                                $item->quality = $item->quality - 2;
                             }
                         }
                     } else {
