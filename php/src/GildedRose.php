606eedada3dad25c013fc597325d1307dd4804c2
frank-hung <frank.hung@kkday.com>
2022-07-01 13:15:32 +0800

    feat: 移除原先程式

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -19,7 +19,6 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-
             switch ($item->name) {
                 case 'Aged Brie':
                     $maxQty = 50;
@@ -61,52 +60,6 @@ final class GildedRose
                         $item->quality = $this->addQty($item->quality, -1, $maxQty, $minQty);
                     }
                     return;
-            }
-
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
-
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
             }
         }
     }
