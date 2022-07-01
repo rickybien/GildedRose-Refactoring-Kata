be67a7a1c282e09892111bbb4a6f5bae4703616c
taihua-chen <taihua.chen@kkday.com>
2022-07-01 11:53:16 +0800

    refactor: 將規則拉成一個個function

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -19,57 +19,75 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros' && $item->name != 'Conjured') {
-                        $item->quality = $item->quality - 1;
-                    }
-                    if ($item->name == 'Conjured') {
-                        $item->quality = max($item->quality - 2, 0);
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
-
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros' && $item->name != 'Conjured') {
-                                $item->quality = $item->quality - 1;
-                            }
-                            if ($item->name === 'Conjured') {
-                                $item->quality = max($item->quality - 2, 0);
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
+            switch ($item->name) {
+                case 'Aged Brie':
+                    $this->updateQualityA($item);
+                    break;
+                case 'Backstage passes to a TAFKAL80ETC concert':
+                    $this->updateQualityB($item);
+                    break;
+                case 'Sulfuras, Hand of Ragnaros':
+                    $this->updateQualityS($item);
+                    break;
+                case 'Conjured':
+                    $this->updateQualityC($item);
+                    break;
+                default:
+                    $this->updateQualityN($item);
+                    break;
             }
         }
+    }
+
+    private function updateQualityA(Item $item): void
+    {
+        $item->sellIn = $item->sellIn -1;
+        $qualityRate = 1;
+        if ($item->sellIn < 0) {
+            $qualityRate *= 2;
+        }
+        $item->quality = min($item->quality + $qualityRate, 50);
+    }
+
+    private function updateQualityB(Item $item): void
+    {
+        $item->sellIn = $item->sellIn -1;
+        $qualityRate = 1;
+        if ($item->sellIn < 0) {
+            $item->quality = 0;
+            return;
+        }
+        if ($item->sellIn < 10) {
+            $qualityRate = 2;
+            if ($item->sellIn < 5) {
+                $qualityRate = 3;
+            }
+        }
+        $item->quality = min($item->quality + $qualityRate, 50);
+    }
+
+    private function updateQualityS(Item $item): void
+    {
+    }
+
+    private function updateQualityC(Item $item): void
+    {
+        $item->sellIn = $item->sellIn -1;
+        $qualityRate = 1;
+        $qualityRate *= 2;
+        if ($item->sellIn < 0) {
+            $qualityRate *= 2;
+        }
+        $item->quality = max($item->quality - $qualityRate, 0);
+    }
+
+    private function updateQualityN(Item $item): void
+    {
+        $item->sellIn = $item->sellIn -1;
+        $qualityRate = 1;
+        if ($item->sellIn < 0) {
+            $qualityRate *= 2;
+        }
+        $item->quality = max($item->quality - $qualityRate, 0);
     }
 }
