f7f451aa72ba1b0778ef955ee50f39c3740f7faa
frank-hung <frank.hung@kkday.com>
2022-07-01 12:50:03 +0800

    feat: 調整 Aged Brie 邏輯

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -19,6 +19,41 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
+
+            switch ($item->name) {
+                case 'Aged Brie':
+                    $maxQty = 50;
+                    $minQty = 0;
+                    --$item->sellIn;
+                    if ($item->sellIn < 0) {
+                        $item->quality = $this->addQty($item->quality, 2, $maxQty, $minQty);
+                    }else {
+                        $item->quality = $this->addQty($item->quality, 1, $maxQty, $minQty);
+                    }
+                    return;
+//                case 'Backstage passes to a TAFKAL80ETC concert':
+//                    $maxQty = 50;
+//                    $minQty = 0;
+//                    --$item->sellIn;
+//                    if ($item->quality < $maxQty) {
+//                        if ($item->sellIn < 10) {
+//                            $item->quality = $this->addQty($item->quality, 2, $maxQty, $minQty);
+//                        } else if ($item->sellIn < 5) {
+//                            $item->quality = $this->addQty($item->quality, 3, $maxQty, $minQty);
+//                        } else if ($item->sellIn < 1) {
+//                            $item->quality = 0;
+//                        } else {
+//                            $item->quality = $this->addQty($item->quality, 1, $maxQty, $minQty);
+//                        }
+//                    }
+//                    return;
+//                case 'Sulfuras, Hand of Ragnaros':
+//                    break;
+//                default:
+//                    $item->sellIn = $item->sellIn - 1;
+//                    break;
+            }
+
             if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality > 0) {
                     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
@@ -65,5 +100,17 @@ final class GildedRose
                 }
             }
         }
+    }
+
+    private function addQty(int $num,int $add, int $max, int $min): int
+    {
+        $result = $num + $add;
+        if ($result > $max) {
+            return $max;
+        }
+        if ($result < $min) {
+            return $min;
+        }
+        return $result;
     }
 }
