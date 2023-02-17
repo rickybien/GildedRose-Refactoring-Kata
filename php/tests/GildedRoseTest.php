acd060775fc69bd8d391b004a494e94781d456da
EddieHuang79 <eddie.huang@kkday.com>
2023-02-17 16:59:30 +0800

    feat: 撰寫通行證邏輯

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -80,4 +80,64 @@ class GildedRoseTest extends TestCase
 			$this->assertSame($exceptedItem['quality'], $items[$index]->quality);
 		}
 	}
+
+	// 測試通行證邏輯
+	public function testBackstagePasses(): void
+	{
+		$name = 'Backstage passes to a TAFKAL80ETC concert';
+		$items = [
+			new Item($name, 0, 30),
+			new Item($name, 20, 30),
+			new Item($name, 10, 30),
+			new Item($name, 5, 30),
+			new Item($name, 20, 50),
+			new Item($name, 10, 49),
+			new Item($name, 5, 48),
+		];
+
+		$excepted = [
+			[
+				'name' => $name,
+				'sellIn' => -1,
+				'quality' => 0,
+			],
+			[
+				'name' => $name,
+				'sellIn' => 19,
+				'quality' => 31,
+			],
+			[
+				'name' => $name,
+				'sellIn' => 9,
+				'quality' => 32,
+			],
+			[
+				'name' => $name,
+				'sellIn' => 4,
+				'quality' => 33,
+			],
+			[
+				'name' => $name,
+				'sellIn' => 19,
+				'quality' => 50,
+			],
+			[
+				'name' => $name,
+				'sellIn' => 9,
+				'quality' => 50,
+			],
+			[
+				'name' => $name,
+				'sellIn' => 4,
+				'quality' => 50,
+			],
+		];
+		$gildedRose = new GildedRose($items);
+		$gildedRose->updateQuality();
+		foreach ($excepted as $index => $exceptedItem) {
+			$this->assertSame($exceptedItem['name'], $items[$index]->name);
+			$this->assertSame($exceptedItem['sellIn'], $items[$index]->sellIn);
+			$this->assertSame($exceptedItem['quality'], $items[$index]->quality);
+		}
+	}
 }
