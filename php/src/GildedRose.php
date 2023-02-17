5836c0424e63ca280304048470ed229eedb5c36b
mingkkday <ming.shih@kkday.com>
2023-02-17 13:55:15 +0800

    refactor to separate "sellIn" and "quality" code

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace GildedRose;
 
+use GildedRose\Item;
+
 final class GildedRose
 {
     /**
@@ -19,51 +21,54 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
-
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-            }
+            $this->sellInModify($item); // 日期先前進，再統一處理品質
+            $this->qualityModify($item);
         }
+    }
+
+    private function sellInModify(Item &$item)
+    {
+        // 永不過期
+        if ($item->name === 'Sulfuras, Hand of Ragnaros') return;
+        
+        $item->sellIn--;
+    }
+
+    private function qualityModify(Item &$item)
+    {
+        $name = $item->name;
+        $quality = &$item->quality;
+        $sellIn = $item->sellIn;
+
+        // 預設品質變化量
+        $qualityModify = -1;
+
+        // 過期後，品質變化速度提升
+        if ($sellIn < 0) $qualityModify *= 2;
+
+        // 客製品質變化量
+        switch ($name) {
+            case 'Sulfuras, Hand of Ragnaros':
+                $qualityModify = 0;
+                break;
+            case 'Aged Brie':
+                if ($sellIn < 0) $qualityModify = 2;
+                else $qualityModify = 1;
+                break;
+            case 'Backstage passes to a TAFKAL80ETC concert':
+                if ($sellIn < 0) $qualityModify = -$quality;
+                elseif ($sellIn < 5) $qualityModify = 3;
+                elseif ($sellIn < 10) $qualityModify = 2;
+                elseif ($sellIn < 49) $qualityModify = 1;
+                break;
+            case 'Conjured':
+                $qualityModify *= 2; // 召喚物，兩倍品質腐敗速度
+                break;
+        }
+
+        $quality += $qualityModify;
+
+        if ($quality < 0) $quality = 0;
+        if ($quality > 50) $quality = 50;
     }
 }
