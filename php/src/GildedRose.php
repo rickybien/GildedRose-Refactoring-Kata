0f21064dc11cf6925cbe4599459cc76e6ce14686
ivankkday <ivan.huang@kkday.com>
2023-02-18 11:35:52 +0800

    refactor

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace GildedRose;
 
-final class GildedRose
+class GildedRose
 {
     /**
      * @var Item[]
@@ -16,54 +16,48 @@ final class GildedRose
         $this->items = $items;
     }
 
-    public function updateQuality(): void
+    public function updateQuality()
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
+            if ($item->name == 'Sulfuras, Hand of Ragnaros') { //"Sulfuras" is a legendary item and as such its Quality is 80 and it never alters.
+                continue;
+            }
+            if ($item->name != 'Aged Brie' && $item->name != 'Backstage passes to a TAFKAL80ETC concert') { //normal items quality degrade
+                $item->quality --;
+                if($item->name == 'Conjured') { //'Conjured' degrades twice as fast as normal items
+                    $item->quality --;
                 }
             } else {
                 if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
+                    $item->quality++;
                     if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                            $item->quality++;
                         }
                         if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                            $item->quality++;
                         }
                     }
                 }
             }
-
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
-
+            $item->sellIn --;
             if ($item->sellIn < 0) {
                 if ($item->name != 'Aged Brie') {
                     if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
+                        $item->quality --;
+                        if($item->name == 'Conjured') {
+                            $item->quality --;
                         }
                     } else {
-                        $item->quality = $item->quality - $item->quality;
+                        $item->quality = 0;
                     }
                 } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
+                    $item->quality ++;
                 }
             }
+            // limit the quality value to the range [0, 50],
+            $item->quality = $item->quality > 0 ? $item->quality : 0;
+            $item->quality = $item->quality < 0 ? $item->quality : 50;
         }
     }
 }
