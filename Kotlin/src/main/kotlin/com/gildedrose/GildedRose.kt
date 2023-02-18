c75b880590342c644b1e2c5a0a8c58e6e81fd72c
Joshua Liu <joshua.liu@kkday.com>
2023-02-18 15:20:49 +0800

    1. 修改測試程式到可以多天工作 2. item 的class 改成data class 3. 先大致調整一下邏輯，把一坨if判斷式拉成數個functions 來處理

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,56 +3,66 @@ package com.gildedrose
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
+        items.forEach { item ->
+            updateItemQuality(item)
+            decreaseSellIn(item)
+            item.takeIf { it.sellIn < 0 }?.apply {
+                updateExpiredItemQuality(this)
             }
         }
     }
 
+
+    private fun updateItemQuality(item: Item) {
+        when (item.name) {
+            "Aged Brie" -> increaseItemQuality(item)
+            "Backstage passes to a TAFKAL80ETC concert" -> updateBackstagePassQuality(item)
+            "Sulfuras, Hand of Ragnaros" -> return
+            else -> decreaseItemQuality(item)
+        }
+    }
+
+    private fun increaseItemQuality(item: Item) {
+        if (item.quality < 50) {
+            item.quality += 1
+        }
+    }
+
+
+    private fun decreaseItemQuality(item: Item) {
+
+        if (item.quality > 0) {
+            item.quality -= 1
+        }
+    }
+
+
+    private fun updateBackstagePassQuality(item: Item) {
+        increaseItemQuality(item)
+        if (item.sellIn < 11) {
+            increaseItemQuality(item)
+        }
+
+        if (item.sellIn < 6) {
+            increaseItemQuality(item)
+        }
+    }
+
+
+
+    private fun decreaseSellIn(item: Item) {
+        if (item.name != "Sulfuras, Hand of Ragnaros") {
+            item.sellIn -= 1
+        }
+    }
+
+
+    private fun updateExpiredItemQuality(item: Item) {
+        when (item.name) {
+            "Aged Brie" -> increaseItemQuality(item)
+            "Backstage passes to a TAFKAL80ETC concert" -> item.quality = 0
+            else -> decreaseItemQuality(item)
+        }
+    }
 }
 
