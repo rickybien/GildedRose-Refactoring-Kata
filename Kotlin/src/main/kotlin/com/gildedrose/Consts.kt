4f2629845de358285fcdffb1833254cca1decca1
Joshua Liu <joshua.liu@kkday.com>
2023-02-18 15:51:33 +0800

    重新最佳化架構，根據情境其實蠻適合使用strategy pattern

---
+++ Kotlin/src/main/kotlin/com/gildedrose/Consts.kt
@@ -0,0 +1,12 @@
+package com.gildedrose
+
+const val MAX_QUALITY = 50
+const val MIN_QUALITY = 0
+
+const val BACKSTAGE_PASS_THRESHOLD_1 = 11
+const val BACKSTAGE_PASS_THRESHOLD_2 = 6
+
+const val AGED_BRIE = "Aged Brie"
+const val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+const val SULFURAS = "Sulfuras, Hand of Ragnaros"
+const val CONJURED = "Conjured Mana Cake"
--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -2,74 +2,17 @@ package com.gildedrose
 
 class GildedRose(val items: Array<Item>) {
 
-    companion object {
-        private const val MAX_QUALITY = 50
-        private const val MIN_QUALITY = 0
-
-        private const val BACKSTAGE_PASS_THRESHOLD_1 = 11
-        private const val BACKSTAGE_PASS_THRESHOLD_2 = 6
-
-        private const val AGED_BRIE = "Aged Brie"
-        private const val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
-        private const val SULFURAS = "Sulfuras, Hand of Ragnaros"
-        private const val CONJURED = "Conjured Mana Cake"
-
-    }
-
     fun updateQuality() {
         items.forEach { item ->
-            updateItemQuality(item)
-            decreaseSellIn(item)
-            if (item.sellIn < 0) {
-                updateExpiredItemQuality(item)
+            val strategy = when (item.name) {
+                AGED_BRIE -> UpdateStrategy.AgedBrieUpdateStrategy
+                BACKSTAGE_PASSES -> UpdateStrategy.BackstagePassUpdateStrategy
+                SULFURAS -> UpdateStrategy.SulfurasUpdateStrategy
+                CONJURED -> UpdateStrategy.ConjuredUpdateStrategy
+                else -> UpdateStrategy.NormalUpdateStrategy
             }
-        }
-    }
-
-    private fun updateItemQuality(item: Item) {
-        when (item.name) {
-            AGED_BRIE -> increaseQuality(item)
-            BACKSTAGE_PASSES -> updateBackstagePassQuality(item)
-            CONJURED -> decreaseQuality(item, 2)
-            SULFURAS -> return
-            else -> decreaseQuality(item)
-        }
-    }
-
-    private fun increaseQuality(item: Item) {
-        if (item.quality < MAX_QUALITY) {
-            item.quality += 1
-        }
-    }
-
-    private fun decreaseQuality(item: Item, decreaseValue: Int = 1) {
-        if (item.quality > MIN_QUALITY && item.name != SULFURAS) {
-            item.quality -= decreaseValue
-        }
-    }
-
-    private fun updateBackstagePassQuality(item: Item) {
-        increaseQuality(item)
-        if (item.sellIn < BACKSTAGE_PASS_THRESHOLD_1) {
-            increaseQuality(item)
-        }
-        if (item.sellIn < BACKSTAGE_PASS_THRESHOLD_2) {
-            increaseQuality(item)
-        }
-    }
-
-    private fun decreaseSellIn(item: Item) {
-        if (item.name != SULFURAS) {
-            item.sellIn -= 1
-        }
-    }
-
-    private fun updateExpiredItemQuality(item: Item) {
-        when (item.name) {
-            AGED_BRIE -> increaseQuality(item)
-            BACKSTAGE_PASSES -> item.quality = MIN_QUALITY
-            CONJURED -> decreaseQuality(item, 2)
-            else -> decreaseQuality(item)
+            strategy.update(item)
         }
     }
 }
+
