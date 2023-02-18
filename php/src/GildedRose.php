f36c44664803c79c0b303e1e200b81c785337d97
Danny <>
2023-02-18 18:12:38 +0800

    feat: 程式碼優化

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -9,7 +9,7 @@ final class GildedRose
     /**
      * @var Item[]
      */
-    private $items;
+    private array $items;
 
     public function __construct(array $items)
     {
@@ -19,64 +19,98 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if($item->name === 'Sulfuras, Hand of Ragnaros'){
+            if ($item->name === 'Sulfuras, Hand of Ragnaros') {
                 $item->quality = 80;
+                continue;
             }
 
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
+            match ($item->name) {
+                'Aged Brie' => $this->updateAgedBrieItem($item),
+                'Backstage passes to a TAFKAL80ETC concert' => $this->updateBackstagePassesItem($item),
+                'Conjured' => $this->updateConjuredItem($item),
+                default => $this->updateNormalItem($item),
+            };
 
-                    if ($item->name === 'Conjured') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+        }
+    }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
+    private function calculateQuality(int $quality, $type = '-'): int
+    {
+        if ($quality <= 0) {
+            return 0;
+        }
 
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
+        if ($type === '-') {
+            $quality--;
+        } else {
+            $quality++;
+        }
 
-                        if ($item->name === 'Conjured') {
-                            $item->quality = $item->quality - 1;
-                        }
-                    }
-                    else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-            }
+        if ($quality > 50) {
+            $quality = 50;
+        }
+
+        return $quality;
+    }
+
+    private function updateNormalItem(Item $item): void
+    {
+        $item->sellIn--;
+        $n = 1;
+        if ($item->sellIn < 0) {
+            $n++;
+        }
+        for ($i = 0; $i < $n; $i++) {
+            $item->quality = $this->calculateQuality($item->quality);
+        }
+
+    }
+
+    private function updateAgedBrieItem(Item $item): void
+    {
+        $item->sellIn--;
+        $n = 1;
+        if ($item->sellIn < 0) {
+            $n++;
+        }
+        for ($i = 0; $i < $n; $i++) {
+            $item->quality = $this->calculateQuality($item->quality, '+');
+        }
+    }
+
+    private function updateBackstagePassesItem(Item $item): void
+    {
+        $item->sellIn--;
+
+        if ($item->sellIn < 0) {
+            $item->quality = 0;
+            return;
+        }
+
+        $n = 1;
+        if ($item->sellIn < 10) {
+            $n++;
+        }
+
+        if ($item->sellIn < 5) {
+            $n++;
+        }
+
+        for ($i = 0; $i < $n; $i++) {
+            $item->quality = $this->calculateQuality($item->quality, '+');
+        }
+    }
+
+    private function updateConjuredItem(Item $item): void
+    {
+        $item->sellIn--;
+        $n = 1;
+        if ($item->sellIn < 0) {
+            $n++;
+        }
+        $n *= 2;
+        for ($i = 0; $i < $n; $i++) {
+            $item->quality = $this->calculateQuality($item->quality);
         }
     }
 }
