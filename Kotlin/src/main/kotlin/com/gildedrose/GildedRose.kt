97dfd9316b729f0217ee2a1edd1ad1565daa9cf7
Joshua Liu <joshua.liu@kkday.com>
2023-02-18 15:21:39 +0800

    1. 把物品字串常數化 2. 把數字相關定義常數化

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,67 +1,71 @@
 package com.gildedrose
 
-class GildedRose(var items: Array<Item>) {
+class GildedRose(val items: Array<Item>) {
+
+    companion object {
+        private const val MAX_QUALITY = 50
+        private const val MIN_QUALITY = 0
+
+        private const val BACKSTAGE_PASS_THRESHOLD_1 = 11
+        private const val BACKSTAGE_PASS_THRESHOLD_2 = 6
+
+        private const val AGED_BRIE = "Aged Brie"
+        private const val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+        private const val SULFURAS = "Sulfuras, Hand of Ragnaros"
+    }
 
     fun updateQuality() {
         items.forEach { item ->
             updateItemQuality(item)
             decreaseSellIn(item)
-            item.takeIf { it.sellIn < 0 }?.apply {
-                updateExpiredItemQuality(this)
+            if (item.sellIn < 0) {
+                updateExpiredItemQuality(item)
             }
         }
     }
 
-
     private fun updateItemQuality(item: Item) {
         when (item.name) {
-            "Aged Brie" -> increaseItemQuality(item)
-            "Backstage passes to a TAFKAL80ETC concert" -> updateBackstagePassQuality(item)
-            "Sulfuras, Hand of Ragnaros" -> return
-            else -> decreaseItemQuality(item)
+            AGED_BRIE -> increaseQuality(item)
+            BACKSTAGE_PASSES -> updateBackstagePassQuality(item)
+            SULFURAS -> return
+            else -> decreaseQuality(item)
         }
     }
 
-    private fun increaseItemQuality(item: Item) {
-        if (item.quality < 50) {
+    private fun increaseQuality(item: Item) {
+        if (item.quality < MAX_QUALITY) {
             item.quality += 1
         }
     }
 
-
-    private fun decreaseItemQuality(item: Item) {
-
-        if (item.quality > 0) {
+    private fun decreaseQuality(item: Item) {
+        if (item.quality > MIN_QUALITY && item.name != SULFURAS) {
             item.quality -= 1
         }
     }
 
-
     private fun updateBackstagePassQuality(item: Item) {
-        increaseItemQuality(item)
-        if (item.sellIn < 11) {
-            increaseItemQuality(item)
+        increaseQuality(item)
+        if (item.sellIn < BACKSTAGE_PASS_THRESHOLD_1) {
+            increaseQuality(item)
         }
-
-        if (item.sellIn < 6) {
-            increaseItemQuality(item)
+        if (item.sellIn < BACKSTAGE_PASS_THRESHOLD_2) {
+            increaseQuality(item)
         }
     }
 
-
-
     private fun decreaseSellIn(item: Item) {
-        if (item.name != "Sulfuras, Hand of Ragnaros") {
+        if (item.name != SULFURAS) {
             item.sellIn -= 1
         }
     }
 
-
     private fun updateExpiredItemQuality(item: Item) {
         when (item.name) {
-            "Aged Brie" -> increaseItemQuality(item)
-            "Backstage passes to a TAFKAL80ETC concert" -> item.quality = 0
-            else -> decreaseItemQuality(item)
+            AGED_BRIE -> increaseQuality(item)
+            BACKSTAGE_PASSES -> item.quality = MIN_QUALITY
+            else -> decreaseQuality(item)
         }
     }
 }
