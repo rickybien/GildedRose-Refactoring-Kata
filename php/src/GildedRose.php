bba14cb247f7e3ec8b6e779925547c58cf813427
HaoChingSui <hao.sui@kkday.com>
2023-02-18 16:01:31 +0800

    refactor: define constant

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace GildedRose;
 
+// 物品名 陳年布利奶酪
+define('HIGHEST_QUALITY', 50);
+define('ITEM_NAME_CHEESE', 'Aged Brie');
+define('ITEM_NAME_PASSES', 'Backstage passes to a TAFKAL80ETC concert');
+define('ITEM_NAME_SULFURAS', 'Sulfuras, Hand of Ragnaros');
+define('PASSES_CLOSE_TO_SELL_DATE', 11);
+define('PASSES_VERY_CLOSE_TO_SELL_DATE', 6);
+
 final class GildedRose
 {
     /**
@@ -19,23 +27,23 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->name != ITEM_NAME_CHEESE and $item->name != ITEM_NAME_PASSES) {
                 if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                    if ($item->name != ITEM_NAME_SULFURAS) {
                         $item->quality = $item->quality - 1;
                     }
                 }
             } else {
-                if ($item->quality < 50) {
+                if ($item->quality < HIGHEST_QUALITY) {
                     $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
+                    if ($item->name == ITEM_NAME_PASSES) {
+                        if ($item->sellIn < PASSES_CLOSE_TO_SELL_DATE) {
+                            if ($item->quality < HIGHEST_QUALITY) {
                                 $item->quality = $item->quality + 1;
                             }
                         }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
+                        if ($item->sellIn < PASSES_VERY_CLOSE_TO_SELL_DATE) {
+                            if ($item->quality < HIGHEST_QUALITY) {
                                 $item->quality = $item->quality + 1;
                             }
                         }
@@ -43,15 +51,15 @@ final class GildedRose
                 }
             }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ($item->name != ITEM_NAME_SULFURAS) {
                 $item->sellIn = $item->sellIn - 1;
             }
 
             if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($item->name != ITEM_NAME_CHEESE) {
+                    if ($item->name != ITEM_NAME_PASSES) {
                         if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                            if ($item->name != ITEM_NAME_SULFURAS) {
                                 $item->quality = $item->quality - 1;
                             }
                         }
@@ -59,7 +67,7 @@ final class GildedRose
                         $item->quality = $item->quality - $item->quality;
                     }
                 } else {
-                    if ($item->quality < 50) {
+                    if ($item->quality < HIGHEST_QUALITY) {
                         $item->quality = $item->quality + 1;
                     }
                 }
