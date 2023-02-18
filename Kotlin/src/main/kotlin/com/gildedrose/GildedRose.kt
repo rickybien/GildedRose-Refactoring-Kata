6107a849474f5a20e1aebc1d51b62c9516178496
Joshua Liu <joshua.liu@kkday.com>
2023-02-18 15:25:28 +0800

    增加"Conjured"（召唤物品）的品质`Quality`下降速度比正常物品快一倍

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -12,6 +12,8 @@ class GildedRose(val items: Array<Item>) {
         private const val AGED_BRIE = "Aged Brie"
         private const val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
         private const val SULFURAS = "Sulfuras, Hand of Ragnaros"
+        private const val CONJURED = "Conjured Mana Cake"
+
     }
 
     fun updateQuality() {
@@ -28,6 +30,7 @@ class GildedRose(val items: Array<Item>) {
         when (item.name) {
             AGED_BRIE -> increaseQuality(item)
             BACKSTAGE_PASSES -> updateBackstagePassQuality(item)
+            CONJURED -> decreaseQuality(item, 2)
             SULFURAS -> return
             else -> decreaseQuality(item)
         }
@@ -39,9 +42,9 @@ class GildedRose(val items: Array<Item>) {
         }
     }
 
-    private fun decreaseQuality(item: Item) {
+    private fun decreaseQuality(item: Item, decreaseValue: Int = 1) {
         if (item.quality > MIN_QUALITY && item.name != SULFURAS) {
-            item.quality -= 1
+            item.quality -= decreaseValue
         }
     }
 
@@ -65,8 +68,8 @@ class GildedRose(val items: Array<Item>) {
         when (item.name) {
             AGED_BRIE -> increaseQuality(item)
             BACKSTAGE_PASSES -> item.quality = MIN_QUALITY
+            CONJURED -> decreaseQuality(item, 2)
             else -> decreaseQuality(item)
         }
     }
 }
-
