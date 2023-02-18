6aa077862a7a9d33cb49777fdd5679f7e2e42af2
Max <max.chiang@kkday.com>
2023-02-18 13:20:15 +0800

    Use Strategy Pattern to refactor Gilded Rose updateQuality

---
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRoseProductType.kt
@@ -0,0 +1,10 @@
+package com.max.strategypattern
+
+enum class GildedRoseProductType(val itemName: String) {
+    PLUS_5_DEXTERITY_VEST("+5 Dexterity Vest"),
+    AGED_BRIE("Aged Brie"),
+    ELIXIR_OF_THE_MONGOOSE("Elixir of the Mongoose"),
+    SULFURAS_HAND_OF_RAGNAROS("Sulfuras, Hand of Ragnaros"),
+    BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT("Backstage passes to a TAFKAL80ETC concert"),
+    CONJURED_MANA_CAKE("Conjured Mana Cake")
+}
--- Kotlin/src/main/kotlin/com/gildedrose/Item.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/Item.kt
@@ -4,4 +4,10 @@ open class Item(var name: String, var sellIn: Int, var quality: Int) {
     override fun toString(): String {
         return this.name + ", " + this.sellIn + ", " + this.quality
     }
+
+    companion object {
+        fun Item.isExpired(): Boolean {
+            return sellIn < 0
+        }
+    }
 }
