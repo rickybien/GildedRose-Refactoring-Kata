00c4be2316dfaec112610be4effe50cfc470270e
Shen <shen.shen@kkday.com>
2023-02-19 16:29:15 +0800

    [refactor] Refactor `GildedRose.updateQuality` func.
    - Add `SpecialItem` enum.

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -6,6 +6,13 @@ class GildedRoseTests: XCTestCase {
         let items = [Item(name: "foo", sellIn: 0, quality: 0)]
         let app = GildedRose(items: items)
         app.updateQuality()
-        XCTAssertEqual(app.items[0].name, "fixme")
+        XCTAssertEqual(app.items[0].name, "foo")
+    }
+    
+    func testItem() throws {
+        let item = Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 2, quality: 80)
+        let app = GildedRose(items: [item])
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 80)
     }
 }
