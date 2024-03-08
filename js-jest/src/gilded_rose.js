da1c7e8d46d16acfee3d1b9889419b5f6a91f24f
Daniel-KK02407 <daniel.li@kkday.com>
2024-03-08 19:40:30 +0800

    feat: Refactor item update logic

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -12,52 +12,64 @@ class Shop {
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros' && !this.items[i].name.includes('Conjured')) {
-            this.items[i].quality = this.items[i].quality - 1;
-          } else if (this.items[i].name.includes('Conjured')) {
+      let productName;
+      if(this.items[i].name.includes('Backstage passes')){
+        productName = 'Backstage passes';
+      } else if(this.items[i].name.includes('Conjured')){
+        productName = 'Conjured';
+      } else if(this.items[i].name.includes('Aged Brie')){
+        productName = 'Aged Brie';
+      } else if(this.items[i].name.includes('Sulfuras')){
+        productName = 'Sulfuras';
+      } else {
+        productName = 'normal';
+      }
+
+      switch (productName) {
+        case 'Aged Brie':
+          if(this.items[i].quality < 50){
+            this.items[i].quality++;
+          }
+          this.items[i].sellIn --;
+          if(this.items[i].sellIn < 0 && this.items[i].quality < 50){
+            this.items[i].quality++;
+          }
+          break;
+        case 'Backstage passes':
+          if(this.items[i].quality < 50){
+            this.items[i].quality++;
+            if(this.items[i].sellIn < 11) {
+              this.items[i].quality++;
+            }
+            if(this.items[i].sellIn < 6) {
+              this.items[i].quality++;
+            }
+          }
+          this.items[i].sellIn --;
+          if(this.items[i].sellIn < 0){
+            this.items[i].quality = 0;
+          }
+          break;
+        case 'Sulfuras':
+          break;
+        case 'Conjured':
+          if (this.items[i].quality > 0) {
             this.items[i].quality = this.items[i].quality - 2;
           }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
+          this.items[i].sellIn--;
+          if(this.items[i].sellIn < 0 && this.items[i].quality > 0){
+            this.items[i].quality = this.items[i].quality - 2;
           }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros' && !this.items[i].name.includes('Conjured')) {
-                this.items[i].quality = this.items[i].quality - 1;
-              } else if (this.items[i].name.includes('Conjured')) {
-                this.items[i].quality = this.items[i].quality - 2;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+          break;
+        default:
+          if (this.items[i].quality > 0) {
+            this.items[i].quality--;
           }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+          this.items[i].sellIn--;
+          if (this.items[i].sellIn < 0 && this.items[i].quality > 0) {
+            this.items[i].quality--;
           }
-        }
+          break;
       }
     }
 
