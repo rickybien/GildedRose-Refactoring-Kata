5e6894791b0df73c0b03d370451920e04c7b8d10
paultsengkkday <paul.tseng@kkday.com>
2024-03-08 15:11:01 +0800

    調整召喚物品未過期前quality下降速度比normal快一倍 召喚物品未過期測試案例 綠燈

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -23,6 +23,9 @@ final class GildedRose
                 if ($item->quality > 0) {
                     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
                         $item->quality = $item->quality - 1;
+                        if ($item->name === 'Conjured') {
+                            $item->quality = $item->quality - 1;
+                        }
                     }
                 }
             } else {
