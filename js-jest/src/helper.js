124d25a94bfd882f331f5b7887baf7b89dabdb5a
Rex Tsou <akccakccwww@gmail.com>
2024-03-08 15:38:31 +0800

    refactor: QUALITY_MIN, QUALITY_MAX constants

--- js-jest/src/helper.js
+++ js-jest/src/helper.js
@@ -1,3 +1,6 @@
+const QUALITY_MIN = 0;
+const QUALITY_MAX = 50;
+
 export const specialItems = {
   AGED_BRIE: 'Aged Brie',
   SULFURAS: 'Sulfuras, Hand of Ragnaros',
@@ -9,6 +12,6 @@ export function checkSpecialItemByName(itemName) {
   return Object.values(specialItems).includes(itemName);
 };
 
-export function clamp(value, min = 0, max = 50) {
+export function clamp(value, min = QUALITY_MIN, max = QUALITY_MAX) {
   return Math.min(Math.max(value, min), max);
 }
