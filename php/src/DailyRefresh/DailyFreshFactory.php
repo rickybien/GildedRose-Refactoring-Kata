8babe02deaf15f3d08d5e743bd9b4d2039cfbc1b
davelai <dave.lai@kkyday.com>
2024-03-08 13:19:57 +0800

    refactor: Item name enum

--- php/src/DailyRefresh/DailyFreshFactory.php
+++ php/src/DailyRefresh/DailyFreshFactory.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 namespace GildedRose\DailyRefresh;
 
+use GildedRose\Enums\ItemName;
+
 class DailyFreshFactory
 {
     public static function createDailyFresh($name): DailyFreshInterface
     {
         return match ($name) {
-            'Aged Brie' => new AgedBrieDailyRefresh(),
-            'Backstage passes to a TAFKAL80ETC concert' => new BackstagePassesTAFKAL80ETCDailyRefresh(),
-            'Sulfuras, Hand of Ragnaros' => new HandofRagnarosDailyRefresh(),
+            ItemName::AGED_BRIE => new AgedBrieDailyRefresh(),
+            ItemName::BACKSTAGE_PASSES => new BackstagePassesTAFKAL80ETCDailyRefresh(),
+            ItemName::HandofRagnaros => new HandofRagnarosDailyRefresh(),
             default => new NormalDailyRefresh(),
         };
     }
---
+++ php/src/Enums/ItemName.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace GildedRose\Enums;
+
+enum ItemName: string
+{
+    case AGED_BRIE = 'Aged Brie';
+    case BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+    case HandofRagnaros = 'Sulfuras, Hand of Ragnaros';
+}
