9b360876c8045d06d3d2fe05ebab529d8d8192bf
paultsengkkday <paul.tseng@kkday.com>
2024-03-08 23:50:11 +0800

    抽出原normal update邏輯至新架構 綠燈

--- php/src/NormalUpdateStrategy.php
+++ php/src/NormalUpdateStrategy.php
@@ -8,5 +8,16 @@ class NormalUpdateStrategy implements UpdateStrategyInterface
 {
     public function update(Item $item): void
     {
+        if ($item->quality > 0) {
+            $item->quality = $item->quality - 1;
+        }
+
+        $item->sellIn = $item->sellIn - 1;
+
+        if ($item->sellIn < 0) {
+            if ($item->quality > 0) {
+                $item->quality = $item->quality - 1;
+            }
+        }
     }
 }
