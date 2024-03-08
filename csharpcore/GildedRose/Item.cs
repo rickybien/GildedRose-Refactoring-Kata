e5d07b631c94e91f1d9af9777252e3beeeabb6ae
Young.Chen <Young.Chen@kkday.com>
2024-03-08 15:37:10 +0800

    優化作業

--- csharpcore/GildedRose/Item.cs
+++ csharpcore/GildedRose/Item.cs
@@ -6,4 +6,16 @@
         public int SellIn { get; set; }
         public int Quality { get; set; }
     }
+    public enum ItemType
+    {
+        AgedBrie,//隨著時間推移而提高品质`Quality`
+        BackstagePasses,//隨著時間推移而提高；還剩10天或更少的时候，品質`Quality`每天提高2；當剩5天或更少的时候，品質`Quality`每天提高3；但一旦過期，品質就會降為0
+
+        Sulfuras,//永不過期，也不會降低品質`Quality`
+        Conjured,//品質比一般正常物品快一倍
+        Normal//一般物品品質會以兩倍速度下降
+        
+        
+        
+    }
 }
