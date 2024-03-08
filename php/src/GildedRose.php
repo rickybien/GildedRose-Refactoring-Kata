f780c8c620293e0cf330ca62dcad692676f4aa32
paultsengkkday <paul.tseng@kkday.com>
2024-03-08 23:39:46 +0800

    調整條件最單純的傳奇物品執行新架構update 綠燈

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -19,6 +19,11 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
+            if ($item->name === 'Sulfuras, Hand of Ragnaros') {
+                UpdateStrategyFactory::create($item->name)->update($item);
+                break;
+            }
+
             if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality > 0) {
                     if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
