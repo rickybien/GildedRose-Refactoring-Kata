31254786e86b71b0f8a16be67740ea7c65448eab
Daniel-KK02407 <daniel.li@kkday.com>
2024-03-08 18:03:53 +0800

    feat: add Conjured product logic for Shop class

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -14,8 +14,10 @@ class Shop {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros' && !this.items[i].name.includes('Conjured')) {
             this.items[i].quality = this.items[i].quality - 1;
+          } else if (this.items[i].name.includes('Conjured')) {
+            this.items[i].quality = this.items[i].quality - 2;
           }
         }
       } else {
@@ -42,8 +44,10 @@ class Shop {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros' && !this.items[i].name.includes('Conjured')) {
                 this.items[i].quality = this.items[i].quality - 1;
+              } else if (this.items[i].name.includes('Conjured')) {
+                this.items[i].quality = this.items[i].quality - 2;
               }
             }
           } else {
