1b3d16eb957b51685c20419b1aa269bbdac07dce
davelai <dave.lai@kkyday.com>
2024-03-08 13:12:59 +0800

    refactor: updateQuality

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace GildedRose;
 
+use GildedRose\DailyRefresh\DailyFreshFactory;
+use GildedRose\DailyRefresh\DailyFreshInterface;
+use GildedRose\DailyRefresh\DailyRefreshInfoDto;
+
 final class GildedRose
 {
     /**
      * @var Item[]
      */
-    private $items;
+    private array $items;
 
     public function __construct(array $items)
     {
@@ -19,51 +23,26 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
-
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-            }
+            $dailyFresh = DailyFreshFactory::createDailyFresh($item->name);
+            $info = $this->getDailyRefreshInfo($dailyFresh, $item);
+            $item->quality = $info->quality;
+            $item->sellIn = $info->sellIn;
         }
+    }
+
+    private function getDailyRefreshInfo(DailyFreshInterface $dailyFresh, Item $item): DailyRefreshInfoDto
+    {
+        $qualityDecreaseRate = $this->getQualityDecreaseRate($item->sellIn);
+        $qualityDecrease = $qualityDecreaseRate * $dailyFresh->qualityDecrease($item->sellIn, $item->quality);
+
+        return new DailyRefreshInfoDto(
+            $item->sellIn - $dailyFresh->sellInDecrease(),
+            $item->quality - $qualityDecrease
+        );
+    }
+
+    private function getQualityDecreaseRate(int $sellIn): int
+    {
+        return $sellIn <= 0 ? 2 : 1;
     }
 }
