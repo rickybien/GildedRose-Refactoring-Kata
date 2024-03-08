e75f02607eb324d8bcfb4b01c5965bd3792943bb
kkguanmin <guanmin.liao@kkday.com>
2024-03-08 14:50:32 +0800

    feat: 新增 更新SellIn & 不同種更新Quality 方法 - updateNormal / updateAgedBrie / updateSulfuras / updateBackstagePass / updateConjured

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -10,7 +10,8 @@ class Shop {
   constructor(items=[]){
     this.items = items;
     this.legendaryItem = 'Sulfuras, Hand of Ragnaros';
-    this.decreaseScale = 1;
+    this.decreaseSellIn = 1;
+    this.decreaseQuality = 1;
     this.qualityMax = 50;
     this.qualityMin = 0;
   }
@@ -62,6 +63,46 @@ class Shop {
     }
 
     return this.items;
+  }
+
+  updateSellIn(item) {
+    if(item.name === this.legendaryItem) return
+    item.sellIn -= this.decreaseSellIn
+  }
+
+  updateNormal(item) {
+    item.sellIn < 0 ? item.quality -= this.decreaseQuality * 2 : item.quality -= this.decreaseQuality
+    item.quality < this.qualityMin ? item.quality = this.qualityMin : item.quality
+  }
+
+  updateAgedBrie(item) {
+    item.sellIn < 0 ? item.quality += this.decreaseQuality * 2 : item.quality += this.decreaseQuality
+    item.quality > this.qualityMax ? item.quality = this.qualityMax : item.quality
+  }
+
+  updateSulfuras(_item) {
+    return
+  }
+
+  updateBackstagePass(item) {
+    const firstSellInStage = 10
+    const secondSellInStage = 6
+
+    if(item.sellIn < 0) {
+      item.quality = 0
+    } else if(item.sellIn < secondSellInStage) {
+      item.quality += this.decreaseQuality * 3
+    } else if(item.sellIn < firstSellInStage) {
+      item.quality += this.decreaseQuality * 2
+    } else {
+      item.quality += this.decreaseQuality
+    }
+    item.quality > this.qualityMax ? item.quality = this.qualityMax : item.quality
+  }
+
+  updateConjured(item) {
+    item.sellIn < 0 ? item.quality -= this.decreaseQuality * 4 : item.quality -= this.decreaseQuality * 2
+    item.quality < this.qualityMin ? item.quality = this.qualityMin : item.quality
   }
 }
 
