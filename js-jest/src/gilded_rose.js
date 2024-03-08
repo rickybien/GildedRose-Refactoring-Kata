597c09ef54fdbf114ff88d14dc3147d30c79cfad
Rex Tsou <akccakccwww@gmail.com>
2024-03-08 15:05:52 +0800

    refactor: for better readability

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -1,3 +1,9 @@
+import {
+  specialItems,
+  checkSpecialItemByName,
+  clamp,
+} from './helper';
+
 class Item {
   constructor(name, sellIn, quality){
     this.name = name;
@@ -12,47 +18,42 @@ class Shop {
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
+      if(this.items[i].name === specialItems.SULFURAS) {
+        return this.items;
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
+
+      this.items[i].sellIn--;
+
+      const item = this.items[i];
+      const { name, sellIn, quality } = item;
+
+      // normal item
+      if(!checkSpecialItemByName(name)) {
+        if(sellIn < 0) {
+          this.items[i].quality = clamp(quality - 2);
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
+          this.items[i].quality = clamp(quality - 1);
+        }
+      }
+
+      // special item
+      if(name === specialItems.AGED_BRIE) {
+        if(sellIn < 0) {
+          this.items[i].quality = clamp(quality + 2);
+        } else {
+          this.items[i].quality = clamp(quality + 1);
+        }
+      }
+
+      if(name === specialItems.BACKSTAGE_PASSES) {
+        if(sellIn < 0) {
+          this.items[i].quality = 0;
+        } else if(sellIn < 5) {
+          this.items[i].quality = clamp(quality + 3);
+        } else if(sellIn < 10) {
+          this.items[i].quality = clamp(quality + 2);
+        } else {
+          this.items[i].quality = clamp(quality + 1);
         }
       }
     }
---
+++ js-jest/src/helper.js
@@ -0,0 +1,13 @@
+export const specialItems = {
+  AGED_BRIE: 'Aged Brie',
+  SULFURAS: 'Sulfuras, Hand of Ragnaros',
+  BACKSTAGE_PASSES: 'Backstage passes to a TAFKAL80ETC concert',
+};
+
+export function checkSpecialItemByName(itemName) {
+  return Object.values(specialItems).includes(itemName);
+};
+
+export function clamp(value, min = 0, max = 50) {
+  return Math.min(Math.max(value, min), max);
+}
