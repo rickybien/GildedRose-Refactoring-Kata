55d05f9a65b764c9978541683263a58b818f61cc
paultsengkkday <paul.tseng@kkday.com>
2024-03-08 23:57:52 +0800

    refactor new normal update 綠燈

--- php/src/NormalUpdateStrategy.php
+++ php/src/NormalUpdateStrategy.php
@@ -8,16 +8,14 @@ class NormalUpdateStrategy implements UpdateStrategyInterface
 {
     public function update(Item $item): void
     {
+        --$item->sellIn;
+
         if ($item->quality > 0) {
-            $item->quality = $item->quality - 1;
+            --$item->quality;
         }
 
-        $item->sellIn = $item->sellIn - 1;
-
-        if ($item->sellIn < 0) {
-            if ($item->quality > 0) {
-                $item->quality = $item->quality - 1;
-            }
+        if ($item->sellIn < 0 && $item->quality > 0) {
+            --$item->quality;
         }
     }
 }
