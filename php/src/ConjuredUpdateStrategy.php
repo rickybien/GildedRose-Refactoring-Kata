9699801dcd923426584a6230c0d228800b71d6bf
paultsengkkday <paul.tseng@kkday.com>
2024-03-09 11:22:49 +0800

    refactor 綠燈

--- php/src/ConjuredUpdateStrategy.php
+++ php/src/ConjuredUpdateStrategy.php
@@ -8,14 +8,14 @@ class ConjuredUpdateStrategy implements UpdateStrategyInterface
 {
     public function update(Item $item): void
     {
-        --$item->sellIn;
-
         if ($item->quality > 0) {
             --$item->quality;
             if ($item->quality > 0) {
                 --$item->quality;
             }
         }
+
+        --$item->sellIn;
 
         if ($item->sellIn < 0 && $item->quality > 0) {
             --$item->quality;
--- php/src/NormalUpdateStrategy.php
+++ php/src/NormalUpdateStrategy.php
@@ -8,11 +8,11 @@ class NormalUpdateStrategy implements UpdateStrategyInterface
 {
     public function update(Item $item): void
     {
-        --$item->sellIn;
-
         if ($item->quality > 0) {
             --$item->quality;
         }
+
+        --$item->sellIn;
 
         if ($item->sellIn < 0 && $item->quality > 0) {
             --$item->quality;
