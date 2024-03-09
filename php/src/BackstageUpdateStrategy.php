fe377e017e62231fedf7db7e4ff249831b2fd0d0
paultsengkkday <paul.tseng@kkday.com>
2024-03-09 11:35:26 +0800

    實作new Backstage update 綠燈

--- php/src/BackstageUpdateStrategy.php
+++ php/src/BackstageUpdateStrategy.php
@@ -8,5 +8,20 @@ class BackstageUpdateStrategy implements UpdateStrategyInterface
 {
     public function update(Item $item): void
     {
+        if ($item->quality < 50) {
+            ++$item->quality;
+            if ($item->sellIn < 11 && $item->quality < 50) {
+                ++$item->quality;
+            }
+            if ($item->sellIn < 6 && $item->quality < 50) {
+                ++$item->quality;
+            }
+        }
+
+        --$item->sellIn;
+
+        if ($item->sellIn < 0) {
+            $item->quality = 0;
+        }
     }
 }
