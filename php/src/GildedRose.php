b703be1e21ddad083b367040e41bbc1ec6fc5f38
paultsengkkday <paul.tseng@kkday.com>
2024-03-09 11:37:45 +0800

    完成new update 移除原update 綠燈

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -19,67 +19,7 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name === 'Sulfuras, Hand of Ragnaros' ||
-                $item->name === 'normal' ||
-                $item->name === 'Conjured' ||
-                $item->name === 'Aged Brie' ||
-                $item->name === 'Backstage passes to a TAFKAL80ETC concert'
-            ) {
-                UpdateStrategyFactory::create($item->name)->update($item);
-                break;
-            }
-
-            if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                        if ($item->name === 'Conjured') {
-                            $item->quality = $item->quality - 1;
-                        }
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
-
-            if ($item->sellIn < 0) {
-                if ($item->name !== 'Aged Brie') {
-                    if ($item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                                if ($item->name === 'Conjured' && $item->quality > 0) {
-                                    $item->quality = $item->quality - 1;
-                                }
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-            }
+            UpdateStrategyFactory::create($item->name)->update($item);
         }
     }
 }
