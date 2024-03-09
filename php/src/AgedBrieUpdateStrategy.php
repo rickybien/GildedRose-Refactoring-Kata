47b495f5923924f4c44e05c858a136c0c654816e
paultsengkkday <paul.tseng@kkday.com>
2024-03-09 11:21:40 +0800

    實作new Aged Brie update 綠燈

--- php/src/AgedBrieUpdateStrategy.php
+++ php/src/AgedBrieUpdateStrategy.php
@@ -8,5 +8,14 @@ class AgedBrieUpdateStrategy implements UpdateStrategyInterface
 {
     public function update(Item $item): void
     {
+        if ($item->quality < 50) {
+            ++$item->quality;
+        }
+
+        --$item->sellIn;
+
+        if ($item->sellIn < 0 && $item->quality < 50) {
+            ++$item->quality;
+        }
     }
 }
