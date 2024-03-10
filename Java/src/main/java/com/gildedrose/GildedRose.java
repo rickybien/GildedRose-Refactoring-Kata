416c189bf8f963c5c793c29619a6c787a65daa40
joanneKKday <anchiao.chang@kkday.com>
2024-03-10 17:27:26 +0800

    加上註解: 辨識物品與更改邏輯

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,25 +9,28 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+                        items[i].quality = items[i].quality - 1; //一般物品, 且還未過期, 降低品質
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
+                // Aged Brie 或 Backstage passes
+                if (items[i].quality < 50) { //品質上限50
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        // Backstage passes
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
+                                // 10天內, 品質再+1
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
 
                         if (items[i].sellIn < 6) {
+                            // 5天內, 品質再+1
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
                             }
@@ -44,14 +47,17 @@ class GildedRose {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
+                            // 一般物品, 且過期, 降低品質
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
                     } else {
+                        // Backstage passes 過期, 品質歸0
                         items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
+                    // Aged Brie 過期, 品質+1
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
                     }
