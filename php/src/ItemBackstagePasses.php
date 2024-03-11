d5f06d20bb0b9ab5c4eef79bd0ed0be353e12cf5
Akira <akira.hong@kkday.com.tw>
2024-03-11 11:25:36 +0800

    feat(PHP):加上 ItemInterface 限制及 type hint

--- php/src/ItemBackstagePasses.php
+++ php/src/ItemBackstagePasses.php
@@ -1,7 +1,9 @@
 <?php
+declare(strict_types=1);
+
 namespace GildedRose;
 
-final class ItemBackstagePasses
+final class ItemBackstagePasses implements ItemInterface
 {
     private $sellIn;
     private $quality;
@@ -10,30 +12,37 @@ final class ItemBackstagePasses
     {
     }
 
-    public function setSellIn($sellIn){
+    public function setSellIn(int $sellIn): void
+    {
         $this->sellIn = $sellIn;
     }
 
-    public function setQuality($quality){
+    public function setQuality(int $quality): void
+    {
         $this->quality = $quality;
     }
 
-    public function getSellIn(){
+
+    public function getSellIn(): int
+    {
         return $this->sellIn;
     }
     
-    public function getQuality(){
+    public function getQuality(): int
+    {
         return $this->quality;
     }
 
-    public function modifySellIn(){
+    public function modifySellIn(): void
+    {
         $this->sellIn -= 1;
         if($this->sellIn < 0){
             $this->quality = $this->quality - $this->quality;
         }
     }
     
-    public function modifyQuality(){
+    public function modifyQuality(): void
+    {
         if($this->quality < 50){
             $this->quality += 1;
             if($this->sellIn < 11){
