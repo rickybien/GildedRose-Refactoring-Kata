03d061aacd07c94a2048d3ac3481d01da7f14f8f
eden.chen <eden.chen@eden.chenMacBook-Pro>
2024-12-05 10:36:13 +0800

    [add] 新增sulfuras物品

---
+++ php/src/Items/Sulfuras.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Sulfuras, Hand of Ragnaros 物品
+ *
+ * @version 0.1.0
+ * @author eden.chen eden.chen@kkday.com
+ * @date 2024/12/4
+ * @since 0.1.0 2024/12/4 eden.chen: 新建立Sulfuras class
+ */
+
+namespace GildedRose\Items;
+
+/**
+ * Sulfuras class 繼承 Normal
+ */
+class Sulfuras extends Normal
+{
+    /**
+     * 原更新品質，但什麼都不做
+     *
+     * @return void
+     */
+    public function triggerUpdateQuality(): void
+    {
+    }
+}
