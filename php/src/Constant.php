9c3a30bac5dd854c89b97d3304a1955bf7046ce0
eden.chen <eden.chen@eden.chenMacBook-Pro>
2024-12-05 10:32:46 +0800

    [fix] 移除不必要的constant

--- php/src/Constant.php
+++ php/src/Constant.php
@@ -2,10 +2,12 @@
 /**
  * 常用的常數
  *
- * @version 0.1.0
+ * @version 0.1.2
  * @author eden.chen eden.chen@kkday.com
  * @date 2024/12/4
  * @since 0.1.0 2024/12/4 eden.chen: 新建立Constant class
+ * @since 0.1.1 2024/12/4 eden.chen: 修正modifier
+ * @since 0.1.2 2024/12/4 eden.chen: 移除不必要的constant
  */
 
 namespace GildedRose;
@@ -16,63 +18,12 @@ namespace GildedRose;
 class Constant
 {
     /**
-     *  品質最大值
-     */
-    const MAX_QUALITY = 50;
-
-    /**
-     *  品質最小值
-     */
-    const MIN_QUALITY = 0;
-
-    /**
-     *  品質變化率
-     */
-    const QUALITY_RATE = [
-        'normal' => 1,
-        'double' => 2,
-    ];
-
-    /**
      *  物品名稱
      */
-    const ITEM = [
+    public const ITEM = [
         'agedBrie' => 'Aged Brie',
         'backstage' => 'Backstage passes to a TAFKAL80ETC concert',
         'conjured' => 'Conjured',
         'sulfuras' => 'Sulfuras, Hand of Ragnaros',
-    ];
-
-    /**
-     *  物品規則
-     */
-    const ITEM_RULE = [
-        'normal' => [
-            'agedBrie' => [
-                'name' => self::ITEM['agedBrie'],
-                'qualityType' => 'increase',
-                'qualityRate' => self::QUALITY_RATE['normal'],
-            ],
-            'backstage' => [
-                'name' => self::ITEM['backstage'],
-                'qualityType' => 'increase',
-                'qualityRate' => self::QUALITY_RATE['normal'],
-            ],
-            'conjured' => [
-                'name' => self::ITEM['conjured'],
-                'qualityType' => 'decrease',
-                'qualityRate' => self::QUALITY_RATE['double'],
-            ],
-            'other' => [
-                'name' => 'Other',
-                'qualityType' => 'decrease',
-                'qualityRate' => self::QUALITY_RATE['normal'],
-            ]
-        ],
-        'legend' => [
-            'sulfuras' => [
-                'name' => self::ITEM['sulfuras'],
-            ]
-        ]
     ];
 }
